20662d4eac7bde82916e8d97d08acf446bca4d19
FreonZ32 <tolyagenii32@gmail.com>
2021-12-21 23:28:33 +0500

    CW

--- DataContainerTwoDirection2.0/Source.cpp
+++ DataContainerTwoDirection2.0/Source.cpp
@@ -131,165 +131,227 @@ public:
 		int& operator*()
 		{return BaseIterator::Temp->Data;}
 	};
-	Iterator begin()
-	{return Head;}
-	Iterator end()
-	{return nullptr;}
+	Iterator begin();
+	Iterator end();
 
-	ConstIterator cbegin()const
-	{return Head;}
-	ConstIterator cend()const
-	{return nullptr;}
+	ConstIterator cbegin()const;
+	ConstIterator cend()const;
 
-	ReverseIterator rbegin()
-	{return Tail;}
-	ReverseIterator rend()
-	{return nullptr;}
+	ReverseIterator rbegin();
+	ReverseIterator rend();
 
-	ConstReverseIterator crbegin()const
-	{return Tail;}
-	ConstReverseIterator crend()const
-	{return nullptr;}
+	ConstReverseIterator crbegin()const;
+	ConstReverseIterator crend()const;
 
-	ForwardList()
-	{
-		Head = Tail = nullptr;
-		size = 0;
-#ifdef DEBUG
-		cout << "Fconstructor:\t" << this << endl;
-#endif // DEBUG
-	}
-	ForwardList(const std::initializer_list<T>& il) :ForwardList()
-	{
-		for (int const* it = il.begin(); it != il.end(); it++)
-		{push_back(*it);}
-	}
-	ForwardList(const ForwardList<T>& other) :ForwardList()
-	{
-		//for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)this->push_back(Temp->Data);
-		*this = other;
-		cout << "FCopyConstructor:\t" << this << endl;
-	}
-	~ForwardList()
-	{
-#ifdef DEBUG
-		while (Head)pop_front();
-		cout << "FDestructor:\t" << this << endl;
-#endif // DEBUG
-	}
+	ForwardList();
+	ForwardList(const std::initializer_list<T>& il);
+	ForwardList(const ForwardList<T>& other);
+	~ForwardList();
 
 	//MODIFICATION
-	void push_front(T Data)
-	{
-		if (Head == nullptr && Tail == nullptr)
-		{
-			Head = Tail = new Element(Data);
-			size++;
-			return;
-		}
-		Head = Head->pPref = new Element(Data, Head);
-		size++;
-	}
-	void push_back(T Data)
-	{
-		if (Head == nullptr && Tail == nullptr)return push_front(Data);
-		Tail = Tail->pNext = new Element(Data, nullptr, Tail);
-		size++;
-	}
+	void push_front(T Data);
+	void push_back(T Data);
 
-	void pop_front()
-	{
-		if (Head == nullptr && Tail == nullptr)return;
-		if (Head == Tail)
-		{
-			delete Head;
-			Head = Tail = nullptr;
-			size--;
-			return;
-		}
-		Head = Head->pNext;
-		delete Head->pPref;
-		Head->pPref = nullptr;
-		size--;
-	}
-	void pop_back()
-	{
-		if (Head == Tail)return pop_front();
-		Tail = Tail->pPref;
-		delete Tail->pNext;
-		Tail->pNext = nullptr;
-		size--;
-	}
+	void pop_front();
+	void pop_back();
 
-	void insert(T Data, int Index)
-	{
-		if (Index >= size)
-		{
-			cout << "Error: Выход за пределы списка!" << endl;
-			return;
-		}
-		Element* Temp;
-		if (Index < size / 2)
-		{Temp = Head;for (int i = 0; i < Index; i++)Temp = Temp->pNext;}
-		else
-		{Temp = Tail;for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPref;}
-		if (Temp == Head) 
-		{push_front(Data); return;}
-		Temp->pPref->pNext = Temp = Temp->pPref = new Element(Data, Temp, Temp->pPref);
-		size++;
-	}
-	void erase(int Index)
-	{
-		if (Index >= size)
-		{
-			cout << "Error: Выход за пределы списка!" << endl;
-			return;
-		}
-		Element* Temp;
-		if (Index < size / 2)
-		{
-			Temp = Head; for (int i = 0; i < Index; i++)Temp = Temp->pNext;
-		}
-		else
-		{
-			Temp = Tail; for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPref;
-		}
-		if (Temp == Head)
-		{pop_front(); return;}
-		if (Temp == Tail)
-		{pop_back(); return;}
-		Temp->pPref->pNext = Temp->pNext;
-		Temp->pNext->pPref = Temp->pPref;
-		delete Temp;
-	}
+	void insert(T Data, int Index);
+	void erase(int Index);
 
 	//METHODS
-	void print()const
-	{
-		cout << "Head: " << "\t" << Head << endl;
-		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
-			cout << Temp->pPref << "\t" << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
-		cout << "Tail: " << "\t" << Tail << endl;
-		cout << "Количество элементов списка: " << size << endl;
-	}
-	void print_reverse()const
-	{
-		cout << "Tail: " << "\t" << Tail << endl;
-		for (Element* Temp = Tail; Temp; Temp = Temp->pPref)
-			cout << Temp->pPref << "\t" << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
-		cout << "Head: " << "\t" << Head << endl;
-		cout << "Количество элементов списка: " << size << endl;
-	}
+	void print()const;
+	void print_reverse()const;
 
 	//OPERATORS
-	ForwardList<T>& operator=(const ForwardList<T>& other)
-	{
-		if (this == &other)return* this;
-		while (Head)pop_front();
-		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)this->push_back(Temp->Data);
-		cout << "FCopyAssignment:\t" << this << endl;
-	}
+	ForwardList<T>& operator=(const ForwardList<T>& other);
 };
+
+template<typename T>
+typename ForwardList<T>::Iterator ForwardList<T>::begin()
+{return Head;}
+template<typename T>
+typename ForwardList<T>::Iterator ForwardList<T>::end()
+{return nullptr;}
+template<typename T>
+typename ForwardList<T>::ConstIterator ForwardList<T>::cbegin()const
+{return Head;}
+template<typename T>
+typename ForwardList<T>::ConstIterator ForwardList<T>::cend()const
+{return nullptr;}
+template<typename T>
+typename ForwardList<T>::ReverseIterator ForwardList<T>::rbegin()
+{return Tail;}
+template<typename T>
+typename ForwardList<T>::ReverseIterator ForwardList<T>::rend()
+{return nullptr;}
+template<typename T>
+typename ForwardList<T>::ConstReverseIterator ForwardList<T>::crbegin()const
+{return Tail;}
+template<typename T>
+typename ForwardList<T>::ConstReverseIterator ForwardList<T>::crend()const
+{return nullptr;}
+
+template<typename T>
+ForwardList<T>::ForwardList()
+{
+	Head = Tail = nullptr;
+	size = 0;
+#ifdef DEBUG
+	cout << "Fconstructor:\t" << this << endl;
+#endif // DEBUG
+}
+template<typename T>
+ForwardList<T>::ForwardList(const std::initializer_list<T>& il) :ForwardList()
+{
+	for (int const* it = il.begin(); it != il.end(); it++)
+	{
+		push_back(*it);
+	}
+}
+template<typename T>
+ForwardList<T>::ForwardList(const ForwardList<T>& other) :ForwardList()
+{
+	//for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)this->push_back(Temp->Data);
+	*this = other;
+	cout << "FCopyConstructor:\t" << this << endl;
+}
+template<typename T>
+ForwardList<T>::~ForwardList()
+{
+#ifdef DEBUG
+	while (Head)pop_front();
+	cout << "FDestructor:\t" << this << endl;
+#endif // DEBUG
+}
+
+//MODIFICATION
+template<typename T>
+void ForwardList<T>::push_front(T Data)
+{
+	if (Head == nullptr && Tail == nullptr)
+	{
+		Head = Tail = new Element(Data);
+		size++;
+		return;
+	}
+	Head = Head->pPref = new Element(Data, Head);
+	size++;
+}
+template<typename T>
+void ForwardList<T>::push_back(T Data)
+{
+	if (Head == nullptr && Tail == nullptr)return push_front(Data);
+	Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+	size++;
+}
+template<typename T>
+void ForwardList<T>::pop_front()
+{
+	if (Head == nullptr && Tail == nullptr)return;
+	if (Head == Tail)
+	{
+		delete Head;
+		Head = Tail = nullptr;
+		size--;
+		return;
+	}
+	Head = Head->pNext;
+	delete Head->pPref;
+	Head->pPref = nullptr;
+	size--;
+}
+template<typename T>
+void ForwardList<T>::pop_back()
+{
+	if (Head == Tail)return pop_front();
+	Tail = Tail->pPref;
+	delete Tail->pNext;
+	Tail->pNext = nullptr;
+	size--;
+}
+template<typename T>
+void ForwardList<T>::insert(T Data, int Index)
+{
+	if (Index >= size)
+	{
+		cout << "Error: Выход за пределы списка!" << endl;
+		return;
+	}
+	Element* Temp;
+	if (Index < size / 2)
+	{
+		Temp = Head; for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+	}
+	else
+	{
+		Temp = Tail; for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPref;
+	}
+	if (Temp == Head)
+	{
+		push_front(Data); return;
+	}
+	Temp->pPref->pNext = Temp = Temp->pPref = new Element(Data, Temp, Temp->pPref);
+	size++;
+}
+template<typename T>
+void ForwardList<T>::erase(int Index)
+{
+	if (Index >= size)
+	{
+		cout << "Error: Выход за пределы списка!" << endl;
+		return;
+	}
+	Element* Temp;
+	if (Index < size / 2)
+	{
+		Temp = Head; for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+	}
+	else
+	{
+		Temp = Tail; for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPref;
+	}
+	if (Temp == Head)
+	{
+		pop_front(); return;
+	}
+	if (Temp == Tail)
+	{
+		pop_back(); return;
+	}
+	Temp->pPref->pNext = Temp->pNext;
+	Temp->pNext->pPref = Temp->pPref;
+	delete Temp;
+}
+
+//METHODS
+template<typename T>
+void ForwardList<T>::print()const
+{
+	cout << "Head: " << "\t" << Head << endl;
+	for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		cout << Temp->pPref << "\t" << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
+	cout << "Tail: " << "\t" << Tail << endl;
+	cout << "Количество элементов списка: " << size << endl;
+}
+template<typename T>
+void ForwardList<T>::print_reverse()const
+{
+	cout << "Tail: " << "\t" << Tail << endl;
+	for (Element* Temp = Tail; Temp; Temp = Temp->pPref)
+		cout << Temp->pPref << "\t" << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
+	cout << "Head: " << "\t" << Head << endl;
+	cout << "Количество элементов списка: " << size << endl;
+}
+
+//OPERATORS
+template<typename T>
+ForwardList<T>& ForwardList<T>::operator=(const ForwardList<T>& other)
+{
+	if (this == &other)return*this;
+	while (Head)pop_front();
+	for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)this->push_back(Temp->Data);
+	cout << "FCopyAssignment:\t" << this << endl;
+}
 
 template<typename T>
 void print_list(const ForwardList<T>& list)
